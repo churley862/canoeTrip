98662eadf3a8ec4e782858a1c10cd163cc508d3b
Collin Hurley <collin.hurley@gmail.com>
2018-11-29 01:56:27 -0500

    Need to add recursive algorithm to find path, have implimented Dijkstra's algorithm to find my fastest path

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -1,199 +1,88 @@
-
-/*
- * Collin Hurley
- * 10/23/2018
- * CS 203
- * */
 package com.company;
 
-import java.util.*;
+import java.util.LinkedList;
 
-public class Graph implements Iterable<Edge> {
-    // nodes stores the number of nodes
-    private int nodes;
-    private int count;
-    private boolean[] matrix;
+public class Graph {
+    int vertices;
+    int currentPos =0;
+    static int matrix[][];
 
-    public Graph(int nodes) {
-        this.nodes = nodes;
-        matrix = new boolean[summation(nodes)];
-    }
-    /**************************************************************/
-    /* Method: Graph */
-    /* Purpose: graph constructor creating an instance of graph
-    /* Parameters: the string to be made into a graph */
-    /* Returns: the boolean if they are equal */
-    /**************************************************************/
-    public Graph(String graph) {
-        Scanner sc = new Scanner(graph);
-
-        if (sc.hasNextInt()) {
-            nodes = sc.nextInt();
-            matrix = new boolean[summation(nodes)];
+    public void addEdge(int source, int destination, int weight) {
+        //add edge
+        if (source < destination) {
+            matrix[source][destination] = weight;
         }
+    }
 
-        while (sc.hasNext()) {
-            String edge = sc.next();
-            String[] nodes = edge.split("[ (),]+");
-            if (nodes.length == 3) {
-                addEdge(Integer.valueOf(nodes[1]), Integer.valueOf(nodes[2]));
+
+    public Graph(int vertex) {
+        this.vertices = vertex;
+        matrix = new int[vertex][vertex];
+    }
+
+    public void print(int source, int [] key){
+        System.out.println("Dijkstra Algorithm: (Adjacency Matrix)");
+        for (int i = 0; i <vertices ; i++) {
+            System.out.println("Source Post: " + source + " to result post " +   + i +
+                    " Cost: " + key[i]);
+        }
+    }
+    int getMinimumVertex(boolean [] mst, int [] key){
+        int minKey = Integer.MAX_VALUE;
+        int vertex = -1;
+        for (int i = 0; i <vertices ; i++) {
+            if(mst[i]==false && minKey>key[i]){
+                minKey = key[i];
+                vertex = i;
             }
         }
+        return vertex;
     }
-    /**************************************************************/
-    /* Method: getNodes*/
-    /* Purpose: node getter method
-    /* Parameters: none */
-    /* Returns: the int node from get */
-    /**************************************************************/
-    public int getNodes() {
-        return nodes;
-    }
-    /**************************************************************/
-    /* Method: hasEdge */
-    /* Purpose: Determines if the edge exists
-    /* comparison for edge class
-    /* Parameters: node1 and node2 */
-    /* Returns: the boolean if the edge exists at the two nodes */
-    /**************************************************************/
-    public boolean hasEdge(int node1, int node2) {
-        if (node1 == node2 || node1 > nodes || node2 > nodes || node1 <= 0 || node2 <= 0) {
-            return false;
-        }
-        return matrix[offset(node1, node2)];
-    }
-    /**************************************************************/
-    /* Method: addEdge */
-    /* Purpose: Adds an edge to the matrix
-    /* Parameters: node1 and node2 */
-    /* Returns: void */
-    /**************************************************************/
-    public void addEdge(int node1, int node2) {
-        if (node1 == node2) return;
+    public void dijkstra_GetMinCost(int source, int numVerticies){
+        boolean[] spt = new boolean[numVerticies];
+        int [] cost = new int[numVerticies];
+        int MAX_INT = Integer.MAX_VALUE;
 
-        final int offset = offset(node1,node2);
-        count += matrix[offset] ? 0 : 1;
-        matrix[offset] = true;
-    }
-    /**************************************************************/
-    /* Method: offset */
-    /* Purpose: returns the offset of the the point in the matrix
-    /* returns the integer offset of the point in the matrix
-    /* Parameters: node1 and node2 */
-    /* Returns: the integer offset of the point in the matrix*/
-    /**************************************************************/
-    protected int offset(int node1, int node2) {
-        if (node1 > node2) {
-            int tmp = node2;
-            node2 = node1;
-            node1 = tmp;
-        }
-        return rowOffset(node1 - 1) + (node2 - node1 - 1);
-    }
-
-    /**************************************************************/
-    /* Method: summation */
-    /* Purpose: summation to help calculate row offset
-    /* Parameters: int node */
-    /* Returns: int total summation */
-    /**************************************************************/
-    protected static int summation(int n) {
-        return n * (n + 1) / 2;
-    }
-    /**************************************************************/
-    /* Method: rowOffset */
-    /* Purpose: calculates the number of rows to offset
-    /* Parameters: node
-    /* Returns: nodes to offset */
-    /**************************************************************/
-    protected int rowOffset(int n) {
-        return n * nodes - summation(n);
-    }
-    /**************************************************************/
-    /* Method: toString */
-    /* Purpose: Converts the graph to a string matrix
-    /* Parameters: none */
-    /* Returns: the String matrix */
-    /**************************************************************/
-    @Override
-    public String toString() {
-        String result = "";
-        for (int row = 1; row <= nodes; ++row) {
-            for (int col = 1; col <= nodes; ++col) {
-                result += (hasEdge(row, col) ? "1 " : "0 ");
-            }
-            result += "\n";
+        //Set the init cost to maximum integer value
+        for (int i = 0; i <numVerticies ; i++) {
+            cost[i] = MAX_INT;
         }
 
-        return result;
-    }
-    /**************************************************************/
-    /* Method: iterator
-    /* Purpose:Overrides the edge iterator to incorporate offsets */
-    /* comparison for edge class
-    /* Parameters: none */
-    /* Returns: An edge iterator  */
-    /**************************************************************/
-    @Override
-    public Iterator<Edge> iterator() {
-        return new Iterator<Edge>() {
-            int found = 0;
-            int pos = -1;
-            int row = 1;
-            int col = 1;
-            /**************************************************************/
-            /* Method: hasNext
-            /* Purpose:Overrides the hasNext function in java to see if
-            /* found is less than count */
-            /* comparison for edge class
-            /* Parameters: none */
-            /* Returns: An edge iterator  */
-            /**************************************************************/
-            @Override
-            public boolean hasNext() {
-                return found < count;
-            }
-            /**************************************************************/
-            /* Method: next
-            /* Purpose:overrides the next method to return the next edge */
-            /* Parameters: none */
-            /* Returns: the next edge instance */
-            /**************************************************************/
-            @Override
-            public Edge next() {
-                // note 0, 0 is always false, so it is safe
-                // to increment before our first check...
-                while (pos < matrix.length) {
-                    col += 1;
-                    if (col > nodes) {
-                        row += 1;
-                        col = row + 1;
-                    }
-                    pos += 1;
-                    if (matrix[pos]) {
-                        found += 1;
-                        return new Edge(row,col);
+        //start from the vertex 0
+        cost[source] = 0;
+
+        //create SPT
+        for (int i = 0; i <numVerticies ; i++) {
+
+            //get the vertex with the minimum cost
+            int vertex_U = getMinimumVertex(spt, cost);
+
+            //include this vertex in SPT
+            spt[vertex_U] = true;
+
+            //iterate through all the adjacent vertices of above vertex and update the keys
+            for (int vertex_V = 0; vertex_V <vertices ; vertex_V++) {
+                //check of the edge between vertex_U and vertex_V
+                if(matrix[vertex_U][vertex_V]>0){
+                    //check if this vertex 'vertex_V' already in spt and
+                    // if cost[vertex_V]!=Infinity
+
+                    if(spt[vertex_V]==false && matrix[vertex_U][vertex_V]!=MAX_INT){
+                        //check if cost needs an update or not
+                        //means check total weight from source to vertex_V is less than
+                        //the current cost value, if yes then update the cost
+
+                        int newKey =  matrix[vertex_U][vertex_V] + cost[vertex_U];
+                        if(newKey<cost[vertex_V])
+                            cost[vertex_V] = newKey;
                     }
                 }
-
-                return null;
-            }
-        };
-    }
-    /**************************************************************/
-    /* Method: edgesAt
-    /* Purpose:returns the edges at a node */
-    /* Parameters: none */
-    /* Returns: returns an iterable of the edges at a node */
-    /**************************************************************/
-    public Iterable<Edge> edgesAt(int n) {
-        Set<Edge> edges = new LinkedHashSet<>();
-
-        for (int i = 1; i <= nodes; ++i) {
-            if (hasEdge(n, i)) {
-                edges.add(new Edge(n, i));
             }
         }
-        return edges;
+        //print shortest path tree
+        print(source, cost);
+        System.out.println("hello");
     }
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,35 +4,50 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
+
         String file = args[0]; // String that holds the file name passed through command line arguments
+        int numVertices = 0;
+
+        Graph graph = null;
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             System.out.println("−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−");
-            int maxCount = Integer.valueOf(reader.readLine());
+            String firstLine = reader.readLine();
+
+            numVertices = Integer.valueOf(firstLine);
+            Graph graph1 = new Graph(numVertices);
             int count = 1;
             String line; // String that holds the line that is read in
             // Reads in all of the lines and prints node1 new line break for each new graph
+            int row = 0;
+            int col = 0;
             while ((line = reader.readLine()) != null) {
-                if (count > 1) {
-                    System.out.println();
+                Scanner strParse = new Scanner(line);
+                while(strParse.hasNextInt()) {
+                    col++;
+                    int temp = strParse.nextInt();
+                    System.out.println("edge added at " + row + "," + col);
+                    graph1.addEdge(row, col, temp);
                 }
-                System.out.println("com.company.Graph" + count + ":");
-                count += 1;
-
-
+                col = row+1;
+                row++;
             }
+            graph1.dijkstra_GetMinCost(0,numVertices);
 
             System.out.println("−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−");
         } catch (FileNotFoundException e) {
             System.out.println("Unable to find file: " + file);
         } catch (IOException e) {
             System.out.println("Error reading " + file + ": " + e);
-        }
 
+            graph = new Graph(numVertices);
+        }
     }
 }
+
